d887a3beabe528ef66d4318b598f53487ca0500a
laorhy <110143146+laorhy@users.noreply.github.com>
2026-02-06 22:50:30 +0800

    Add ROS2 bringup for spray sim

--- spray_robot_bringup/setup.py
+++ spray_robot_bringup/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/spray_sim.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'spray_sim_bringup = spray_robot_bringup.spray_sim_bringup:main',
         ],
     },
 )
